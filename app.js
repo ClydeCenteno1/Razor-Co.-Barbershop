b70fa9326b5b85dbd8fb0ace52f162721bbeb493
ClydeCenteno1 <https://github.com/ClydeCenteno1>
2026-06-25 23:12:20 -0400

    Enhance mobile navigation with transition effects and update service descriptions for clarity

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const mobileNav = document.querySelector("#mobileNav")
 const hamburgerIcon = document.querySelector("#hamburgerIcon")
 
 hamburgerIcon.addEventListener("click", () => {
-    mobileNav.classList.toggle("hidden")
+    mobileNav.classList.toggle("max-h-0")
+    mobileNav.classList.toggle("opacity-0")
+    mobileNav.classList.toggle("max-h-96")
+    hamburgerIcon.classList.toggle("rotate-90")
 })
